8ce4e2161b39fae170172de575edf02638e4c9f1
Mark Choi <admin@raimsoft.com>
2018-12-21 23:51:42 +0700

    M: Redundant by Redandancy checker M: EmptyLinesRemover and DuplicateRedundancyRemover functions in python code.

--- redundancy_checker.py
+++ redundancy_checker.py
@@ -1,27 +1,56 @@
 import os 
 from os import listdir
 from os.path import isfile, join
+from collections import OrderedDict
 
-mypath = os.getcwd() + "\\KoreanList"
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+MYPATH = os.getcwd() + "\\KoreanList"
+onlyfiles = [f for f in listdir(MYPATH) if isfile(join(MYPATH, f))]
 
-for each in onlyfiles:
-	new_temp= []
-	target_file= mypath+"\\"+each
-	name, ext = os.path.splitext(each)
+def EmptyLineRemover():
+	EmptyLines= 0
+	for each in onlyfiles:
+		new_temp= []
+		target_file= MYPATH+"\\"+each
+		name, ext = os.path.splitext(each)
 
-	if(ext == ".txt"):
-		f = open(target_file,"r")
-		lines = f.readlines()
-		f.close()
+		if(ext == ".txt"):
+			f = open(target_file,"r")
+			lines = f.readlines()
+			f.close()
 
-		for line in lines:
-			if line == "\n":
-				continue
-			else:
-				new_temp.append(line)
+			for line in lines:
+				if line == "\n":
+					EmptyLines = EmptyLines + 1
+					continue
+				else:
+					new_temp.append(line)
 
-		f = open(target_file,"w")
-		for line in new_temp:
-			f.write(line)
-		f.close()
+			f = open(target_file,"w")
+			for line in new_temp:
+				f.write(line)
+			f.close()
+		print(str(EmptyLines) + " empty lines are reduced in filename:" + each)
+
+def DuplicateRedundancyRemover():
+	for each in onlyfiles:
+		new_temp= []
+		target_file= MYPATH+"\\"+each
+		name, ext = os.path.splitext(each)
+
+		if(ext == ".txt"):
+			f = open(target_file,"r")
+			lines = f.readlines()
+			f.close()
+
+			RemovedSet= list(set(lines))
+
+			f = open(target_file,"w")
+			for line in RemovedSet:
+				f.write(line)
+			f.close()
+
+		print(str(len(lines) - len(RemovedSet)) + " duplicated lines are reduced in filename:" + each)
+	
+
+EmptyLineRemover()
+DuplicateRedundancyRemover()
